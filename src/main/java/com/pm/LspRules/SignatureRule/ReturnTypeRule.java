279951e8ce0437e400896bb48b71c5ab46b78a70
anushka2890 <anushka28903@gmail.com>
2025-09-13 21:54:56 +0530

    return type rule of signature rule of lsp

---
+++ src/main/java/com/pm/LspRules/SignatureRule/ReturnTypeRule.java
@@ -0,0 +1,53 @@
+package com.pm.LspRules.SignatureRule;
+
+// Return Type Rule :
+// Subtype overridden method return type should be either identical
+// or narrower than the parent method's return type.
+// This is also called return type covariance.
+// Java supports this out of the box.
+
+class Animal {
+    // some common Animal methods
+}
+
+class Dog extends Animal {
+    // Additional Dog methods specific to Dogs.
+}
+
+class ParentRT {
+    public Animal getAnimal() {
+        System.out.println("Parent : Returning Animal instance");
+        return new Animal();
+    }
+}
+
+class ChildRT extends ParentRT {
+    @Override
+    public Animal getAnimal() {
+        System.out.println("Child : Returning Dog instance");
+        return new Dog();
+    }
+}
+
+class ClientRT {
+    private ParentRT p;
+
+    public ClientRT(ParentRT p) {
+        this.p = p;
+    }
+
+    public void takeAnimal() {
+        p.getAnimal();
+    }
+}
+
+public class ReturnTypeRule {
+    public static void main(String[] args) {
+        ParentRT parent = new ParentRT();
+        ChildRT child   = new ChildRT();
+
+        ClientRT client = new ClientRT(child);
+        //Client client = new Client(parent);
+        client.takeAnimal();
+    }
+}
